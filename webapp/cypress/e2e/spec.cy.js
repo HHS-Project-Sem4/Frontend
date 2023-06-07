2dd19fb0d8843525de9382ae0450743e06031941
Sander <sander.heidenis@gmail.com>
2023-06-07 16:27:17 +0200

    added page visiting tests

--- webapp/cypress/e2e/spec.cy.js
+++ webapp/cypress/e2e/spec.cy.js
@@ -1,8 +1,46 @@
-describe('template spec', () => {
+describe('page visits', () => {
 
-  it('visit page', () => {
-    cy.visit('http://localhost:3000')
-    expect(true).to.equal(true)
+  it('default page verkoop', () => {
+    cy.contains('VERKOOP OVERZICHT')
+  })
+
+  it('visit page kosten', () => {
+    let button = cy.get('button')
+
+    button.contains('KOSTEN').click()
+
+    cy.contains('KOSTEN OVERZICHT')
+  })
+
+  it('visit page winst', () => {
+    let button = cy.get('button')
+
+    button.contains('WINST').click()
+
+    cy.contains('WINST OVERZICHT')
+  })
+
+  it('visit page opslag', () => {
+    let button = cy.get('button')
+
+    button.contains('OPSLAG').click()
+
+    cy.contains('OPSLAG OVERZICHT')
+  })
+  it('visit page klanten', () => {
+    let button = cy.get('button')
+
+    button.contains('KLANTEN').click()
+
+    cy.contains('KLANTEN OVERZICHT')
+  })
+
+  it('visit page filialen', () => {
+    let button = cy.get('button')
+
+    button.contains('FILIALEN').click()
+
+    cy.contains('FILIALEN OVERZICHT')
   })
 
 
